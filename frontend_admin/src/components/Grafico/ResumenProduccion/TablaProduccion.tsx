325fe7c1772f7a70f495b87e3579482422460590
Juan Manuel Castro <j.manuelcastro@hotmail.com>
2025-05-05 07:51:48 -0300

    vs

--- frontend_admin/src/components/Grafico/ResumenProduccion/TablaProduccion.tsx
+++ frontend_admin/src/components/Grafico/ResumenProduccion/TablaProduccion.tsx
@@ -1,4 +1,3 @@
-import * as XLSX from 'xlsx';
 import { ResumenProduccion } from "@/types/grafico";
 import { ROLES_USUARIOS } from "@/constants/rolesUsuarios";
 
@@ -10,60 +9,8 @@ interface Props {
 
 export default function TablaProduccion(props: Props) {
 
-  function exportarExcel() {
-    const tabla = document.querySelector("#tabla-produccion") as HTMLTableElement;
-    if (!tabla) return;
-  
-    const ws = XLSX.utils.table_to_sheet(tabla);
-    const range = XLSX.utils.decode_range(ws['!ref'] || '');
-  
-    for (let R = range.s.r; R <= range.e.r; ++R) {
-      for (let C = range.s.c; C <= range.e.c; ++C) {
-        const cellAddress = XLSX.utils.encode_cell({ r: R, c: C });
-        const cell = ws[cellAddress];
-        if (!cell) continue;
-  
-        cell.s = {
-          border: {
-            top:    { style: "thin", color: { rgb: "000000" } },
-            bottom: { style: "thin", color: { rgb: "000000" } },
-            left:   { style: "thin", color: { rgb: "000000" } },
-            right:  { style: "thin", color: { rgb: "000000" } },
-          },
-          font: {
-            bold: R === 0,
-          },
-          alignment: {
-            horizontal: "center",
-            vertical: "center",
-          },
-          fill: R === 0
-            ? {
-                patternType: "solid",
-                fgColor: { rgb: "F3F4F6" }, // gris claro
-              }
-            : undefined,
-        };
-      }
-    }
-  
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "Producción");
-    XLSX.writeFile(wb, "Reporte_Produccion.xlsx");
-  }
-  
-
   return (
     <>
-      <div class="flex justify-end">
-        <button
-          onClick={exportarExcel}
-          class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-        >
-          Exportar a Excel
-        </button>
-      </div>
-
       <div class="overflow-x-auto mt-5">
         <table id="tabla-produccion" class="w-full table-auto border border-collapse text-sm">
           <thead class="bg-gray-100">
