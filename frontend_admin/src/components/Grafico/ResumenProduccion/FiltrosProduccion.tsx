57ad38ffe592777732852eee4ffdc7eaeda1cb85
Juan Manuel Castro <j.manuelcastro@hotmail.com>
2025-05-05 08:18:11 -0300

    vs

--- frontend_admin/src/components/Grafico/ResumenProduccion/FiltrosProduccion.tsx
+++ frontend_admin/src/components/Grafico/ResumenProduccion/FiltrosProduccion.tsx
@@ -1,6 +1,8 @@
 import { createSignal, onMount, onCleanup, Show, For } from "solid-js";
 import { buscarProductosPorTexto } from "@/services/producto.service";
 import InputFecha from "@/components/Layout/InputFecha";
+import { Download } from 'lucide-solid';
+import { exportarTablaAExcel } from '@/utils/exportarTabla';
 
 interface Props {
   desde: string;
@@ -162,7 +164,14 @@ export default function FiltrosProduccion(props: Props) {
       </select>
 
       {/* Botón limpiar */}
-      <div class="w-full flex justify-center md:justify-end">        
+      <div class="w-full flex justify-center md:justify-end"> 
+      <button
+                onClick={() => exportarTablaAExcel('tabla-produccion', 'Reporte Produccion')}
+                class="flex items-center gap-2 bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+              >
+                <Download size={18} />
+                Exportar Reporte
+              </button>       
         <button
           onClick={props.limpiarFiltros}
           class="bg-gray-400 text-white px-4 py-2 rounded w-full md:w-auto"
