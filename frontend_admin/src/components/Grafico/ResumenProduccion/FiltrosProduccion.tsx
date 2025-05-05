6f83872e5b4db3610decb8e29bd337fdfd11a442
Juan Manuel Castro <j.manuelcastro@hotmail.com>
2025-05-05 08:59:45 -0300

    vs

--- frontend_admin/src/components/Grafico/ResumenProduccion/FiltrosProduccion.tsx
+++ frontend_admin/src/components/Grafico/ResumenProduccion/FiltrosProduccion.tsx
@@ -165,20 +165,22 @@ export default function FiltrosProduccion(props: Props) {
       </select>
 
       {/* Botones */}
-      <div class="w-full flex justify-end gap-2 md:col-span-4">
+      <div class="w-full md:col-span-4 flex flex-col md:flex-row justify-end gap-2 mt-2">
         <button
           onClick={props.onExportar}
-          class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+          class="flex items-center justify-center gap-2 bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 w-full md:w-auto"
         >
-          <Download size={18} /> Exportar Reporte
+          <Download size={18} />
+          Exportar Reporte
         </button>
         <button
           onClick={props.limpiarFiltros}
-          class="bg-gray-400 text-white px-4 py-2 rounded hover:bg-gray-500"
+          class="bg-gray-400 text-white px-4 py-2 rounded hover:bg-gray-500 w-full md:w-auto"
         >
           Limpiar
         </button>
       </div>
+
     </div>
   );
 }
