d0cdcd52dd48c70a293aefdff06849127e93886c
Juan Manuel Castro <j.manuelcastro@hotmail.com>
2025-05-27 15:32:39 -0300

    v

--- backend/controllers/producto.controller.js
+++ backend/controllers/producto.controller.js
@@ -490,7 +490,7 @@ export async function obtenerProductosRelacionadosPorTexto(texto = '', limite = 
   const productos = await Producto.findAll({
     where,
     include,
-    limit,
+    limit: Number(limite), // 💥 acá fallaba
     order: [['precioUnitario', 'ASC']],
   });
 
