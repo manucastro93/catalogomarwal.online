0cdb3dc6145970512635f22e1e7529922166f13c
manucastro93 <j.manuelcastro@hotmail.com>
2025-06-17 12:27:15 -0300

    v

--- frontend_admin/src/components/ProductosPedidosPendientes/TablaProductosPendientes.tsx
+++ frontend_admin/src/components/ProductosPedidosPendientes/TablaProductosPendientes.tsx
@@ -14,7 +14,7 @@ export default function TablaProductosPendientes(props: {
             <table class="w-full text-sm border-collapse">
                 <thead class="bg-gray-100 sticky top-0">
                     <tr>
-                        {(["cod_item", "descripcion", "cantidad_pedida", "cantidad_facturada", "cantidad_pendiente", "stock"] as (keyof ProductoPendiente)[]).map((col) => (
+                        {(["cod_item", "descripcion", "cantidad_pedida", "cantidad_facturada", "cantidad_pendiente", "stock", "fabricar"] as (keyof ProductoPendiente)[]).map((col) => (
                             <th
                                 class="text-left p-3 border-b cursor-pointer whitespace-nowrap"
                                 onClick={() => props.onOrdenar(col)}
@@ -48,6 +48,7 @@ export default function TablaProductosPendientes(props: {
                                     <td class="p-3">{formatearMiles(p.cantidad_facturada)}</td>
                                     <td class="p-3 font-bold text-red-600">{formatearMiles(p.cantidad_pendiente)}</td>
                                     <td class="p-3">{formatearMiles(p.stock)}</td>
+                                    <td class="p-3">{formatearMiles(p.cantidad_pendiente - p.stock)}</td>
                                 </tr>
                             )}
                         </For>
