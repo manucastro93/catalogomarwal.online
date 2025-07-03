24f1374c07fcf0006a8727c3b84bfcdfcd2d2e20
manucastro93 <j.manuelcastro@hotmail.com>
2025-07-03 13:42:12 -0300

    v

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: 'server',
       script: './server.js',
       cwd: __dirname,
-      watch: false, // Cambiar a true si querés que reinicie en desarrollo al guardar
+      watch: false,
       ignore_watch: ['node_modules', 'public', 'logs'],
       env: {
         NODE_ENV: 'production',
@@ -15,5 +15,31 @@ module.exports = {
       merge_logs: true,
       time: true,
     },
+    {
+      name: 'sync_dux_12hs',
+      script: './sincronizarPedidosFacturasDux.js',
+      cwd: __dirname,
+      cron_restart: '0 8-16 * * *', // Cada hora de 8 a 16hs
+      out_file: './logs/sync_dux_12hs-out.log',
+      error_file: './logs/sync_dux_12hs-error.log',
+      merge_logs: true,
+      time: true,
+      env: {
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      name: 'sync_dux_18hs',
+      script: './sincronizarProductosDux.js',
+      cwd: __dirname,
+      cron_restart: '0 18 * * *', // Una vez a las 18hs
+      out_file: './logs/sync_dux_18hs-out.log',
+      error_file: './logs/sync_dux_18hs-error.log',
+      merge_logs: true,
+      time: true,
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
   ],
 };
