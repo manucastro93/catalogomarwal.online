df8236a946599d7ab64412bd81d2e6da7c1a07e5
manucastro93 <j.manuelcastro@hotmail.com>
2025-07-21 12:31:49 -0300

    v

--- backend/scripts/importar_clientes_dux.py
+++ backend/scripts/importar_clientes_dux.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from playwright.async_api import async_playwright
 
 # 📦 Cargar variables de entorno
 load_dotenv(dotenv_path=Path(__file__).resolve().parents[1] / ".env")
